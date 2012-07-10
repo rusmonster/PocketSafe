90c3d35eb66c2ad03b7b5d1e02d57ec9e20af852
Dmitry Kalita <kalita@cryptogsm.ru>
2012-07-10 11:12:30 +0300

    fix notificator update

--- SmsSafe/src/com/softmo/smssafe/SmsMainActivity.java
+++ SmsSafe/src/com/softmo/smssafe/SmsMainActivity.java
@@ -26,7 +26,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
